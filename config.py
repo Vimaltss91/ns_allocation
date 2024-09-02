b5aa1a2528d143c9ef126992c112ca3b5afbe655
vimalchand TS <vimaltss91@gmail.com>
2024-09-02 16:47:33 +0530

    test

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 # Database configuration
-DB_HOST = os.getenv("DB_HOST", "")
-DB_USER = os.getenv("DB_USER", "")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_NAME = os.getenv("DB_NAME", "")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 # Supported actions
 ACTIONS = ['insert_or_update', 'allocate_namespace', 'delete']
